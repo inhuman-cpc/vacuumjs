3f76a29697aca5f4d0f2b7dd31f49cc43a961df6
simongfxu <xugaofan@gmail.com>
2016-08-29 16:37:39 +0800

    使用babel重构

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vacuumjs
 
 A node.js web page content extractor
+
+[![Build Status](https://semaphoreci.com/api/v1/damngoto/vacuumjs/branches/master/badge.svg)](https://semaphoreci.com/damngoto/vacuumjs)
--- test/index.js
+++ test/index.js
@@ -1,25 +1,36 @@
-var fs = require('fs')
-var urlList = [
-  // 'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/constructing-the-object-model?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/render-tree-construction?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/render-blocking-css?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/adding-interactivity-with-javascript?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/analyzing-crp?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/optimizing-critical-rendering-path?hl=zh-cn',
-  'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/page-speed-rules-and-recommendations?hl=zh-cn'
-]
-var proxy = 'socks://127.0.0.1:1080'
-var parse5 = require('parse5')
+import fs from 'fs'
+import parse5 from 'parse5'
+import {expect} from 'chai'
+import extract from '../lib/extract'
+import utils from '../lib/utils'
 
-var vacuumjs = require('../src/index')
-vacuumjs.parse(urlList, {
-  proxy: proxy,
-  referenceUrl: 'https://developers.google.com/web/fundamentals/performance/critical-rendering-path/?hl=zh-cn'
-}).then(nodes => {
-  var htmlList = nodes.map(node => {
-    return vacuumjs.serialize(node)
+function split(array, index) {
+  return array.filter((item, i) => i !== index)
+}
+
+// 提取系列文章：https://developers.google.com/web/fundamentals/performance/critical-rendering-path/?hl=zh-cn
+let samples = []
+for (let i = 1; i < 9; i += 1) {
+  samples[i - 1] = parse5.parse(fs.readFileSync(`${__dirname}/../sample/${i}.html`, 'utf8'))
+}
+
+describe('#extract', function() {
+  it('should get null when reference dom is null or the same', function() {
+    let node = extract(samples[0], samples[0])
+    let node2 = extract(samples[0])
+    expect(node).to.be.null
+    expect(node2).to.be.null
   })
-  fs.writeFile('./index.html', htmlList.join('\n\n********\n\n'))
+
+  it('should get the content node that has a class named page-content', function() {
+    for (let i = 0; i < 8; i += 1) {
+      let refDOM = samples[i]
+      let targets = split(samples, i)
+      targets.forEach((dom, index) => {
+        let contentNode = extract(dom, refDOM)
+        expect(contentNode).to.not.be.null
+        expect(contentNode.attrs[0].value).equal('page-content')
+      })
+    }
+  })
 })
